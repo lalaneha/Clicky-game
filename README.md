f57396f36975af401f98e22deb1c7f36bf0c35ef
Neha Lal <rani259@hotmail.com>
2019-11-01 10:03:59 -0700

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Clicky-Game
-
-This is a simple memory challenge game written in React
-
-Don't click the same card twice!
--- src/components/wrapper/wrapper.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Wrapper = props => <div className="container-fluid">{props.children}</div>;
-
-export default Wrapper;
