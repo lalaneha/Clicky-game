24145b1e088dfc648de2eebe4eb929ed86abaa27
Neha Lal <rani259@hotmail.com>
2019-11-01 10:01:29 -0700

    Updates

---
+++ precache-manifest.a04683f197016522e12119faed31046d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818a437c53a4fdd4d1e317cab19b1ee1",
+    "url": "/lalaneha/Clicky-game/index.html"
+  },
+  {
+    "revision": "256316fb35b44b341621",
+    "url": "/lalaneha/Clicky-game/static/css/main.0260f581.chunk.css"
+  },
+  {
+    "revision": "452bd16d96bfa482163d",
+    "url": "/lalaneha/Clicky-game/static/js/2.bc877cb2.chunk.js"
+  },
+  {
+    "revision": "256316fb35b44b341621",
+    "url": "/lalaneha/Clicky-game/static/js/main.22a1b81e.chunk.js"
+  },
+  {
+    "revision": "bd09651501717a429183",
+    "url": "/lalaneha/Clicky-game/static/js/runtime-main.c995d6a2.js"
+  }
+]);
